46a9ba4a0defb801075dffed271b60d2fb74a843
Shane Graff <copellius@gmail.com>
2021-08-12 14:35:07 -0700

    WIP: Add async makeAPICall() to index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyConversion from './currency_conversion.js';
 
+function getElements(response) {
+  if (response) {
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+async function makeApiCall() {
+  const response = await CurrencyConversion.getConversion(currency);
+  getElements(response);
+}
 
 $('#convert').click(function() {
   let conversion = $('#currencyToConvert').val();
   console.log(conversion);
-  // clearFields()
+  makeApiCall(currency);
 });
