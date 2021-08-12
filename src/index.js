312c5869bc1716b9752908efb01d3f26eeb7c1ed
Shane Graff <copellius@gmail.com>
2021-08-12 14:16:51 -0700

    Add input field to index.html for amount of currency to convert

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-$;
+
+
+$('#enterUsCurrency').click(function() {
+  let conversion = $('#convert').val();
+  // clearFields()
+});
