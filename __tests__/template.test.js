628f618c7a2b98c0a1d14a22f3151e3b7f54213b
Shane Graff <copellius@gmail.com>
2021-08-14 12:20:01 -0700

    Add test for CurrencyConversion object

---
+++ __tests__/template.test.js
@@ -0,0 +1,15 @@
+import CurrencyConversion from '../src/js/currencyConversion.js'
+
+describe('CurrencyConversion', () => {
+  let currency;
+
+  beforeEach(() => {
+    currency = new CurrencyConversion(20.00, 76.00);
+  });
+
+  test("should create a currency object with the currency amount in US dollars and the amount of US dollars in selected foreign currency", () => {
+    expect(currency.us).toEqual(20);
+    expect(currency.foreign).toEqual(76);
+    expect(currency.usToForeignCurrencyConverter()).toEqual(1520.00);
+  });
+});
